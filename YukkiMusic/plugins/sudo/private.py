5d2e6729c3efd1f941df08d23c79f10480667205
alielsayed76 <98778110+alielsayed76@users.noreply.github.com>
2022-06-11 22:04:13 +0200

    Update private.py

--- YukkiMusic/plugins/sudo/private.py
+++ YukkiMusic/plugins/sudo/private.py
@@ -25,7 +25,7 @@ UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
 AUTHORIZED_COMMAND = get_command("AUTHORIZED_COMMAND")
 
 
-@app.on_message(filters.command(AUTHORIZE_COMMAND) & SUDOERS)
+@app.on_message(filters.command(AUTHORIZE_COMMAND,None) & SUDOERS)
 @language
 async def authorize(client, message: Message, _):
     if config.PRIVATE_BOT_MODE != str(True):
@@ -43,7 +43,7 @@ async def authorize(client, message: Message, _):
         await message.reply_text(_["pbot_5"])
 
 
-@app.on_message(filters.command(UNAUTHORIZE_COMMAND) & SUDOERS)
+@app.on_message(filters.command(UNAUTHORIZE_COMMAND,None) & SUDOERS)
 @language
 async def unauthorize(client, message: Message, _):
     if config.PRIVATE_BOT_MODE != str(True):
@@ -61,7 +61,7 @@ async def unauthorize(client, message: Message, _):
         return await message.reply_text(_["pbot_4"])
 
 
-@app.on_message(filters.command(AUTHORIZED_COMMAND) & SUDOERS)
+@app.on_message(filters.command(AUTHORIZED_COMMAND,None) & SUDOERS)
 @language
 async def authorized(client, message: Message, _):
     if config.PRIVATE_BOT_MODE != str(True):
